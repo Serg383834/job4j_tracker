d2b109a2f678d0b60caa560b29a2cdd09d43a11c
serg383834 <serg383834@yandex.ru>
2021-12-14 08:49:39 +0300

    1.1. Создание объекта [#165452 #9798]

---
+++ src/main/java/ru/job4j/oop/Dog.java
@@ -0,0 +1,11 @@
+package ru.job4j.oop;
+
+public class Dog {
+
+    public static void main(String[] args) {
+        Dog sharik = new Dog();
+        Dog laika = new Dog();
+        Dog bobik = new Dog();
+    }
+
+}
