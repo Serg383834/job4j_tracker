499677506cfe2ec10a95ab66bb9e5cd4f3a2d591
Serg383834 <serg383834@yandex.ru>
2022-08-15 18:27:21 +0300

    6. Метод замены заявки. Tracker.replace [#211748 #9812] Не решен

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -19,15 +19,10 @@ public class Tracker {
     /*проверяет в цикле все элементы массива items, сравнивая id с аргументом int id
 и возвращает найденный Item. Если Item не найден - возвращает null.*/
     public Item findById(int id) {
-        Item rsl = null;
-        for (int i = 0; i < size; i++) {
-            Item item = items[i];
-            if (item.getId() == id) {
-                rsl = item;
-                break;
-            }
-        }
-        return rsl;
+        /* Находим индекс */
+        int index = indexOf(id);
+        /* Если индекс найден возвращаем item, иначе null */
+        return index != -1 ? items[index] : null;
     }
 
     // возвращает копию массива items без null элементов (без пустых ячеек).
@@ -48,6 +43,35 @@ getName класса Item) с аргументом метода String key. Эл
             }
         }
         return Arrays.copyOf(itemsWithoutNull, size);
+    }
+
+    /*Реализуйте метод Tracker.replace, используя метод indexOf.
+      Причем вызов indexOf должен быть один.*/
+    public boolean replace(int id, Item item) {
+    /*Первоначально нужно найти ячейку с id приходящем в параметре.
+      Мы уже реализовали методы для поиска по id,
+      но этот метод возвращает объект Item, а не index.
+      Давайте создадим новый метод, который будет возвращать index по id.*/
+        //indexOf(id);
+        boolean bul = false;
+        if (findById(id) != null) {
+            items[indexOf(id)] = item;
+            //items[indexOf(id)].setId(id);
+            bul = true;
+        }
+
+        return bul;
+    }
+
+    private int indexOf(int id) {
+        int rsl = -1;
+        for (int index = 0; index < size; index++) {
+            if (items[index].getId() == id) {
+                rsl = index;
+                break;
+            }
+        }
+        return rsl;
     }
 
 }
